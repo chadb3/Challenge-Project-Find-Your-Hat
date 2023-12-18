1da6a8774fa3a484604a0392e8fa59d1deac8127
Chad B <chadb78999@gmail.com>
2023-12-18 08:30:30 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Challenge-Project-Find-Your-Hat
 Challenge Project: Find Your Hat (Node.js)
+
+The attached npm modules are
+ - terminal-kit
+ - Modules provided by codecademy
+ 	-prompt-sync
+ 		- above required - prompt-sync-history (to run test at least)
+	- ansi-regex
+	- strip-ansi
+
---
+++ main.js
@@ -0,0 +1,9 @@
+// I included the codecademy provided files due to the below.
+// I am not sure if I need it or not.
+// I'll remove the files if i don't use them...
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
